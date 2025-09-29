71e6d4e2eba96a8e5d10b54ddb1e10ce49e7f3b0
Ritik Negi <negiritik47@gmail.com>
2025-09-29 18:50:01 +0530

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="container">
+    <h2>{{ user.age }}</h2>
+    <h3>{{ user.name }}</h3>
+    <button @click="setAge">Change Age</button>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { ref } from 'vue';
+import { reactive } from 'vue';
+// import func from 'vue-editor-bridge';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    // const uName = ref('Maximilian');
+    // const uAge = ref(31);
+    const user = reactive({
+      name: 'Ritik',
+      age: 20,
+    });
+
+    setTimeout(function () {
+      user.name = 'Ritik';
+      user.age = 22;
+    });
+
+    return { user: user };
+  },
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //     age: 31
+  //   };
+  // },
+  // methods: {
+  //   setNewAge() {
+  //     this.age = 32;
+  //   }
+  // }
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
